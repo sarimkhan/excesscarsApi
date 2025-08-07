868f87989437c71a0e938cc67d12c2b7c07b33ae
Sarim Khan <sarimkhan2work@gmail.com>
2025-08-06 20:39:28 -0500

    Update excesscarsApi.py

--- excesscarsApi.py
+++ excesscarsApi.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
