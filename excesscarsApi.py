7b320547e4f455973f01f40eec202a8ebf790389
Sarim Khan <sarimkhan2work@gmail.com>
2025-08-20 17:37:56 -0500

    Update excesscarsApi.py

--- excesscarsApi.py
+++ excesscarsApi.py
@@ -32,7 +32,7 @@ def getAllVehicles():
 def getAllFeaturedVehicles():
     conn = psycopg2.connect(connectionString)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM vehicles WHERE CAST(price as INT) < 15000;")
+    cur.execute("SELECT * FROM vehicles WHERE CAST(price as INT) < 15000 LIMIT 5;")
     tableVals = cur.fetchall()
     return tableVals;
 def getAllVehicleWithVIN(vin=""):
